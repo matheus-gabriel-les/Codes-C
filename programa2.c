c8329907db6ed911fcdd620b560552ddf06f0d91
Matheus Gabriel Lino <100940909+matheus-gabriel-les@users.noreply.github.com>
2025-08-14 10:45:42 -0300

    loop criado em case 3

--- programa2.c
+++ programa2.c
@@ -39,16 +39,27 @@ int main(){
         break;
 
     case 3 :
-        
+         do
+         {
          printf("Digite um numero: ");
          scanf("%f", &valor1);
          printf("Digite outro numero: ");
          scanf("%f", &valor2);
+             
+         if (valor2 != 0)
+            {
+                resultado = valor1 / valor2 ;
+                printf("Resultado da operacao : %.2f\n", resultado);
+                break;
+            }
+            else
+             { 
+                printf("Valor invalido!\n");
+             }
 
-         resultado = valor1 / valor2 ;
-         printf("Resultado da operacao : %.2f", resultado );
-
-        break;
+         }while(1);
+          return 0;
+        
         
     case 4 :
             printf("Digite um numero: ");
@@ -76,3 +87,4 @@ int main(){
       
     
 }
+
