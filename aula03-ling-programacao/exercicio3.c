8793ba515871a0e2256cd331dcc1879be7e133d5
Matheus Gabriel Lino <100940909+matheus-gabriel-les@users.noreply.github.com>
2025-08-26 16:14:29 -0300

    Update exercicio3.c

--- aula03-ling-programacao/exercicio3.c
+++ aula03-ling-programacao/exercicio3.c
@@ -2,40 +2,38 @@
 #include <string.h>
 #include <stdbool.h>
 
-int main(){
+int main()
+{
 
-    int numeros[4],i,aux[4],j;
-    bool compara;
-    int maior = 0;
+    int numeros[2][2];
+    int i, j;
+    int maiorValor;
 
-
-    for ( i = 0; i < 4; i++)
+    for (i = 0; i < 2; i++)
     {
-        printf("Digite um numero:  ");
-        scanf("%d",&numeros[i]);
-
+        for (j = 0; j < 2; j++)
+        {
+            printf("Digite um numero: ");
+            scanf("%d", &numeros[i][j]);
+        }
     }
 
-    for ( i = 0; i < 2; i++)
+    maiorValor = numeros[0][0];
+
+    for (i = 0; i < 2; i++)
     {
-       for ( i = 0; i < 2; i++)
-       {
-          printf("%d",numeros[i]);
-          printf("  ");
-          printf("%d\n",numeros[i + 1]);
-
-          aux[j] = numeros[i];//variavel auxiliar recebe o numero salvo no vetor numeros
-          compara = aux[j] > numeros[i +1];//comapara recebe verdadeiro ou falso se o numero em aux e maior que o proximo numero do vetor  
-          
-          if(compara){
-
-            maior = aux[j];//se for verdadeira maior recebe aux
-          }
-       }      
-       
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d ", numeros[i][j]);
+            if (numeros[i][j] > maiorValor)
+            {
+                 maiorValor = numeros[i][j];
+            }
+            
+        }
+        printf("\n");
     }
-    
-     
-   printf("O maior elemento da matriz e:  %d\n",maior);
-    
+
+    printf("Maior valor da matriz: %d ", maiorValor);
+    return 0;
 }
